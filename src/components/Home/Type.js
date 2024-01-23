531c1e195a9960ac19f4550bf45f81a5dcbd9704
tech-key academy <techkeyacademy@gmail.com>
2024-01-23 19:37:04 +0530

    added description

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "8+ years of Experience",
+          "currently building AI based Security Software",
+          "Instructor",
         ],
         autoStart: true,
         loop: true,
