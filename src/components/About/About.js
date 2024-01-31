99fef9c9b4a337cdc533f0c1a3f13b9a39659ccd
tech-key academy <techkeyacademy@gmail.com>
2024-02-01 01:49:14 +0530

    About section changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              Know Who <strong className="purple"> WE ARE</strong>
             </h1>
             <Aboutcard />
           </Col>
@@ -35,13 +35,14 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Professional <strong className="purple">Skillset </strong> You Earn
         </h1>
 
         <Techstack />
 
         <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+          <strong className="purple">Tools </strong> 
+          You Get To Know
         </h1>
         <Toolstack />
 
--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,7 +6,7 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
+        Days We Have <strong className="purple">Coded</strong>
       </h1>
       <GitHubCalendar
         username="tech-key-academy"
