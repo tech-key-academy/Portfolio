336dd800cb73307acbd5aebc0ea26c51d91f55ef
tech-key academy <techkeyacademy@gmail.com>
2024-02-20 00:41:43 +0530

    removed description

--- src/components/Courses/CourseCards.js
+++ src/components/Courses/CourseCards.js
@@ -7,9 +7,6 @@ function CourseCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
       </Card.Body>
     </Card>
   );
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -26,7 +26,6 @@ function Courses() {
               imgPath={chatify}
               isBlog={false}
               title="Full Stack Software Engineering"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
             />
           </Col>
 
@@ -35,7 +34,6 @@ function Courses() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Web Backend Developer"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
             />
           </Col>
 
@@ -44,7 +42,6 @@ function Courses() {
               imgPath={editor}
               isBlog={false}
               title="Web Frontend Developer"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"              
             />
           </Col>
 
@@ -53,7 +50,6 @@ function Courses() {
               imgPath={leaf}
               isBlog={false}
               title="Software Testing"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
             />
           </Col>
 
@@ -62,7 +58,6 @@ function Courses() {
               imgPath={suicide}
               isBlog={false}
               title="Android App Development"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
             />
           </Col>
 
@@ -71,8 +66,6 @@ function Courses() {
               imgPath={emotion}
               isBlog={false}
               title="IOS App Development"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person." 
             />
           </Col>
         </Row>
